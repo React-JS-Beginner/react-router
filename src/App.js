a5284a3978cfdf66f552f34c0e94fd6a468b5204
avishekataur <avishekataur@gmail.com>
2021-10-01 06:48:13 +0600

    not-found-done

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import Contacts from "./components/Contacts/Contacts";
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
+import NotFound from "./components/NotFound/NotFound";
 import Posts from "./components/Posts/Posts";
 import Users from "./components/Users/Users";
 
@@ -13,21 +14,31 @@ function App() {
       <Router>
         <Header></Header>
         <Switch>
+
           <Route exact path="/">
             <Home></Home>
           </Route>
+
           <Route path="/home">
             <Home></Home>
           </Route>
+
           <Route path="/posts">
             <Posts></Posts>
           </Route>
+
           <Route path="/users">
             <Users></Users>
           </Route>
+
           <Route path="/contacts">
             <Contacts></Contacts>
           </Route>
+
+          <Route path="*">
+            <NotFound></NotFound>
+          </Route>
+
         </Switch>
       </Router>
     </div>
